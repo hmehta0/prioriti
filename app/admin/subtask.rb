c18a8a97ed491b07320a620da97c2baf843a76dd
Raghu Betina <raghu@firstdraft.com>
2017-11-25 22:44:08 +0000

    Whitelist columns in Subtask for ActiveAdmin

--- app/admin/subtask.rb
+++ app/admin/subtask.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Subtask do
 
+ permit_params :subtaskname, :subtaskprioriti, :subtaskduedate, :subtaskstatus, :task_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #

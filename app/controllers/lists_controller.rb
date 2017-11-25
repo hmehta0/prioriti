0746c17fb52eadb9d416aebf89c3dfb96ef0d458
Raghu Betina <raghu@firstdraft.com>
2017-11-25 22:44:11 +0000

    Only list user can modify list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,14 @@
 class ListsController < ApplicationController
+  before_action :current_user_must_be_list_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_list_user
+    list = List.find(params[:id])
+
+    unless current_user == list.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @lists = List.all
 
